451bb3b7b210e7934bc3889f6718e2f01b61221e
oigami <oigamishunta@gmail.com>
2016-02-19 12:08:59 +0900

    画像のurlを修正

--- app/src/main/java/com/example/oigami/twimpt/twimpt/TwimptUtil.java
+++ app/src/main/java/com/example/oigami/twimpt/twimpt/TwimptUtil.java
@@ -6,8 +6,8 @@ package com.example.oigami.twimpt.twimpt;
 public class TwimptUtil {
   public static final String TWIMPT_API_URL = "http://api.twimpt.com/";
   public static final String TWIMPT_TWIST_URL = "http://twist.twimpt.com/";
-  public static final String TWIMPT_IMAGE_URL = "http://twimpt.com/upload/original/";
-  public static final String TWIMPT_ICON_URL = "http://twimpt.com/icon/";
+  public static final String TWIMPT_IMAGE_URL = "http://old.twimpt.com/upload/original/";
+  public static final String TWIMPT_ICON_URL = "http://old.twimpt.com/icon/";
   public static final String TWIMPT_THUMBNAIL_IMAGE_URL = "http://twimpt.com/upload/thumbnail/";
 }
 
